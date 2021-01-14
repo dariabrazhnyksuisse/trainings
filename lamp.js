6a64c99f9192b758f94fe825c9567aeadfb3a030
dariabrazhnyksuisse <daria.brazhnyk.suisse@gmail.com>
2021-01-14 23:51:24 +0100

    hello

---
+++ lamp.js
@@ -0,0 +1,19 @@
+/**
+ * @Date:   2021-01-12T22:57:31+01:00
+ * @Last modified time: 2021-01-14T23:50:55+01:00
+ */
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+
+</body>
+</html>
